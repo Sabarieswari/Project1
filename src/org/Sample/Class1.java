a342373722d06791298a5d5a29d1116e549fbf5a
Sabarieswari <sabargit27@gmail.com>
2020-06-17 22:32:38 +0530

    Save Success

---
+++ src/org/Sample/Class1.java
@@ -0,0 +1,13 @@
+package org.Sample;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+	
+	
+	@Test
+	 public void Method1(){
+		System.out.println("Method1 is executed with Thread Id:");
+	
+	 }
+}
